950fec279b5cc6f90b18a2e558390c36c99d345d
marcopolello <marco-pol@hotmail.it>
2020-12-17 17:47:34 +0100

    commit 2

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log("hello world!!");
+console.log("hello world!!"); // GOAL: in NUOVO progetto NPM, definire semplice layout con header, footer, navbar e content, posizionati correttamente, ogniuno con un colore di background differente e con una scritta in centro che ne riporti il tipo di elemento (header, footer, ecc). Si raccomanda di sfruttare il SASS il piu' possibile, con particolare attenzione alla suddivisione in files dei fogli di stile.
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
 console.log("hello world!!");
+
+// GOAL: in NUOVO progetto NPM, definire semplice layout con header, footer, navbar e content, posizionati correttamente, ogniuno con un colore di background differente e con una scritta in centro che ne riporti il tipo di elemento (header, footer, ecc). Si raccomanda di sfruttare il SASS il piu' possibile, con particolare attenzione alla suddivisione in files dei fogli di stile.
